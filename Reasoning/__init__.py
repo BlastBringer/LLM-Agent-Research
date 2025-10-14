459017f4075d62e4fcbe20d61eeb81ae3831bb67
KarthikGS <131282327+k-thikgs@users.noreply.github.com>
2025-10-14 12:09:24 +0530

    Template creation

--- Reasoning/__init__.py
+++ Reasoning/__init__.py
@@ -1,2 +1,6 @@
 # Reasoning Module
-# This module will contain all reasoning-related components
+# This module contains all reasoning-related components
+
+from .templatizer import WordProblemTemplatizer, templatize_word_problem, TemplatizationResult
+
+__all__ = ['WordProblemTemplatizer', 'templatize_word_problem', 'TemplatizationResult']
